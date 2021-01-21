f80326813bd04a0ecbeb89760ca85263163f1ec2
matub2009 <77771283+matub2009@users.noreply.github.com>
2021-01-21 15:23:59 +0700

    Add files via upload

---
+++ MyAccountPage.cs
@@ -0,0 +1,21 @@
+﻿using BCQAExam.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace BCQAExam.Pages
+{
+    public class MyAccountPage:BasePage
+    {
+        private IWebElement AccountName => Driver.FindControl(By.XPath("//a[@title='View my customer account']"));
+        private IWebElement MyAccountTab => Driver.FindControl(By.XPath("//span[@class='navigation_page']"));
+
+        public void AssertAccountName()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("Lorena De Canas", AccountName.Text);
+                Assert.AreEqual("My account", MyAccountTab.Text);
+            });
+        }
+    }
+}
---
+++ NavigationPage.cs
@@ -0,0 +1,21 @@
+﻿using BCQAExam.Core;
+using OpenQA.Selenium;
+using System.Configuration;
+
+namespace BCQAExam.Pages
+{
+    public class NavigationPage : BasePage
+    {
+        public void NavigateToHomePage()
+        {
+            Driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["URL"]);
+        }
+
+        private IWebElement SignInButton => Driver.FindControl(By.XPath("//a[@class='login']"));
+
+        public void NavigateToLoginPage()
+        {
+            SignInButton.Click();
+        }
+    }
+}
